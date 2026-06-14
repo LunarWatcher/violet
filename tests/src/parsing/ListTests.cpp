c2be4d1c71ebb0f997c70da06aa92ce9752762a8
Olivia <oliviawolfie@pm.me>
2026-06-14 05:14:42 +0200

    Separate tests of lists

---
+++ tests/src/parsing/ListTests.cpp
@@ -0,0 +1,107 @@
+#include "violet/parsing/Markdown.hpp"
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Unordered lists", "[markdown]") {
+    SECTION("Plain bullet lists") {
+        std::stringstream ss;
+        ss << "* Bullet *1*\n"
+           << "* Bullet **2**";
+        REQUIRE(
+            violet::Markdown::parse(ss)
+            ==
+            "<ul><li><p>Bullet <em>1</em></p></li><li><p>Bullet <strong>2</strong></p></li></ul>"
+        );
+    }
+    SECTION("Bullet lists with multiple paragraphs") {
+        std::stringstream ss;
+        ss << "* Bullet *1*\n"
+           << "  Line continuation\n"
+           << "\n"
+           << "  extra paragraph\n"
+           << "* Bullet **2**";
+        REQUIRE(
+            violet::Markdown::parse(ss)
+            ==
+            "<ul>"
+                "<li><p>Bullet <em>1</em> Line continuation</p>"
+                    "<p>extra paragraph</p>"
+                "</li>"
+                "<li><p>Bullet <strong>2</strong></p></li>"
+            "</ul>"
+        );
+    }
+    SECTION("Bullet list with code") {
+        std::stringstream ss;
+        ss << "* Test\n"
+           << "  ```cpp\n"
+           << "  code content\n"
+           << "  ```\n"
+           << "* Second point";
+        REQUIRE(
+            violet::Markdown::parse(ss)
+            ==
+            "<ul>"
+                "<li>"
+                    "<p>Test</p>"
+                    "<pre><code class=\"language-cpp\">code content</code></pre>"
+                "</li>"
+                "<li>"
+                    "<p>Second point</p>"
+                "</li>"
+            "</ul>"
+        );
+    }
+    SECTION("Bullet list with code directly under bullet") {
+        std::stringstream ss;
+        ss << "* ```cpp\n"
+           << "  code content\n"
+           << "  ```\n"
+           << "* Second point";
+        REQUIRE(
+            violet::Markdown::parse(ss)
+            ==
+            "<ul>"
+                "<li>"
+                    "<pre><code class=\"language-cpp\">code content</code></pre>"
+                "</li>"
+                "<li>"
+                    "<p>Second point</p>"
+                "</li>"
+            "</ul>"
+        );
+    }
+}
+
+TEST_CASE("Ordered lists", "[markdown]") {
+    SECTION("Plain numbered lists") {
+        std::stringstream ss;
+        ss << "1. Bullet *1*\n"
+           << "2. Bullet **2**";
+        REQUIRE(
+            violet::Markdown::parse(ss)
+            ==
+            "<ol><li><p>Bullet <em>1</em></p></li><li><p>Bullet <strong>2</strong></p></li></ol>"
+        );
+    }
+    SECTION("Numbered list with code") {
+        std::stringstream ss;
+        ss << "1. Test\n"
+           << "  ```cpp\n"
+           << "  code content\n"
+           << "  ```\n"
+           << "2. Second point";
+        REQUIRE(
+            violet::Markdown::parse(ss)
+            ==
+            "<ol>"
+                "<li>"
+                    "<p>Test</p>"
+                    "<pre><code class=\"language-cpp\">code content</code></pre>"
+                "</li>"
+                "<li>"
+                    "<p>Second point</p>"
+                "</li>"
+            "</ol>"
+        );
+    }
+}
